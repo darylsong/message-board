546a2c68604d6458f3801ca0d10fc3f18a7cc930
Daryl Song <songlwdaryl@gmail.com>
2020-10-09 01:22:13 +0800

    initial commit

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const server = http.createServer(app)
 
 const PORT = process.env.PORT || 3003
 
-server.listen(config.PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
